cce9e7efa313aad50333078cf773927d7749aa6b
Paul Brighton <brighton.paul@yahoo.com>
2018-09-09 13:51:56 +0100

    Most css styling added for home page.

---
+++ app/assets/javascripts/hamburger.js
@@ -0,0 +1,7 @@
+function openNav () {
+  document.querySelector("[data-behavior='myNav']").style.width = '100%';
+}
+
+function closeNav () {
+  document.querySelector("[data-behavior='myNav']").style.width = '0%';
+}
