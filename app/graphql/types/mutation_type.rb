2659aff54fc34e63e53294ce7fce5038b09946f3
Paul Brighton <brighton.paul@yahoo.com>
2019-01-03 12:49:42 +0000

    Feature/graphql mutations (#77)
    * Graphql comment mutations working but rspec test needs sorting
    
    * Added Graphql mutation for adding comments to an article
    
    * Removed article and article_id from comment_type.rb and updated test

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,4 +1,17 @@
 module Types
   class MutationType < Types::BaseObject
+    field :newComment, CommentType, null: true do
+      description "Adding a new comment to an article"
+      argument :author_name, String, required: true
+      argument :body, String, required: true
+      argument :image, String, required: true
+      argument :slug, String, required: true
+    end
+
+    def new_comment(slug:, author_name:, body:, image:)
+      
+      article = Article.friendly.find(slug)
+      article.comments.create(author_name: author_name, body: body, image: image)
+    end
   end
 end
